48d2610d2c286e968a65e6fc7578248573e59f35
Sam <sblausten@Sams-Mac.local>
2017-05-08 15:23:47 +0100

    First "hopefully" failing test

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,10 @@
+describe("fizzBuzz", function() {
+
+  beforeEach(function() {
+  });
+
+  it("should print numbers 1 to 100 in order", function() {
+    expect(fizzBuzz.print()).toContain(1..100);
+  });
+});
+
